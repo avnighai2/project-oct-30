00e3c32a504554cd86923ca2eb858ffa31f3c200
avnighai2 <avnighai2@gmail.com>
2024-10-30 19:53:08 -0400

    alphabetornumber

---
+++ alphabetornumber.py
@@ -0,0 +1,8 @@
+# Input: Character from user
+char = input("Enter a character: ")
+
+# Check if the character is an alphabet
+if char.isalpha():
+    print(f"{char} is an alphabet.")
+else:
+    print(f"{char} is not an alphabet.")
